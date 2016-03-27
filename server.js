ef45508a9b9b7bb5d6d8963973060429a4d0cdf2
Luis <luispa91@gmail.com>
2016-03-27 17:15:01 -0600

    Updated ADAL auth code

--- server.js
+++ server.js
@@ -1,26 +1,70 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
+'use strict';
 
-var adal = require('adal-node').AuthenticationContext;
+var fs = require('fs');
+var adal = require('adal-node');
 
-var authorityHostUrl = 'https://login.windows.net';
-var tenant = 'myTenant';
-var authorityUrl = authorityHostUrl + '/' + tenant;
-var clientId = 'yourClientIdHere';
-var clientSecret = 'yourAADIssuedClientSecretHere'
+
+var AuthenticationContext = adal.AuthenticationContext;
+
+function turnOnLogging() {
+  var log = adal.Logging;
+  log.setLoggingOptions(
+  {
+    level : log.LOGGING_LEVEL.VERBOSE,
+    log : function(level, message, error) {
+      console.log(message);
+      if (error) {
+        console.log(error);
+      }
+    }
+  });
+}
+
+/*
+ * You can override the default account information by providing a JSON file
+ * with the same parameters as the sampleParameters variable below.  Either
+ * through a command line argument, 'node sample.js parameters.json', or
+ * specifying in an environment variable.
+ * {
+ *    "tenant" : "rrandallaad1.onmicrosoft.com",
+ *    "authorityHostUrl" : "https://login.windows.net",
+ *    "clientId" : "624ac9bd-4c1c-4687-aec8-b56a8991cfb3",
+ *    "clientSecret" : "verySecret=""
+ * }
+ */
+var parametersFile = process.argv[2] || process.env['ADAL_SAMPLE_PARAMETERS_FILE'];
+
+var sampleParameters;
+if (parametersFile) {
+  var jsonFile = fs.readFileSync(parametersFile);
+  if (jsonFile) {
+    sampleParameters = JSON.parse(jsonFile);
+  } else {
+    console.log('File not found, falling back to defaults: ' + parametersFile);
+  }
+}
+
+if (!parametersFile) {
+  sampleParameters = {
+    tenant : 'rrandallaad1.onmicrosoft.com',
+    authorityHostUrl : 'https://login.windows.net',
+    clientId : '624ac9bd-4c1c-4687-aec8-b56a8991cfb3',
+    clientSecret : ''
+  };
+}
+
+var authorityUrl = sampleParameters.authorityHostUrl + '/' + sampleParameters.tenant;
+
 var resource = '00000002-0000-0000-c000-000000000000';
- 
+
+turnOnLogging();
+
 var context = new AuthenticationContext(authorityUrl);
- 
-context.acquireTokenWithClientCredentials(resource, clientId, clientSecret, function(err, tokenResponse) {
+
+context.acquireTokenWithClientCredentials(resource, sampleParameters.clientId, sampleParameters.clientSecret, function(err, tokenResponse) {
   if (err) {
     console.log('well that didn\'t work: ' + err.stack);
   } else {
     console.log(tokenResponse);
   }
 });
-
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Adal Node\n');
-}).listen(port);
