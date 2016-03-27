e618bd40b4a70414d1dfda60572d4949fe8facd2
Luis <luispa91@gmail.com>
2016-03-27 16:55:57 -0600

    Added adal-node dependancy to server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure\n');
-}).listen(port);
+var adal = require('adal-node').AuthenticationContext;
+
+var authorityHostUrl = 'https://login.windows.net';
+var tenant = 'myTenant';
+var authorityUrl = authorityHostUrl + '/' + tenant;
+var clientId = 'yourClientIdHere';
+var clientSecret = 'yourAADIssuedClientSecretHere'
+var resource = '00000002-0000-0000-c000-000000000000';
+ 
+var context = new AuthenticationContext(authorityUrl);
+ 
+context.acquireTokenWithClientCredentials(resource, clientId, clientSecret, function(err, tokenResponse) {
+  if (err) {
+    console.log('well that didn\'t work: ' + err.stack);
+  } else {
+    console.log(tokenResponse);
+  }
+});
