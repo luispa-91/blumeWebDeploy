6165478a54a5b44c97e4510c8e2b10ed8244573f
Luis <luispa91@gmail.com>
2016-03-27 17:04:45 -0600

    Remove ADAL

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 
-var adal = require('adal-node').AuthenticationContext;
-
-var authorityHostUrl = 'https://login.windows.net';
-var tenant = 'myTenant';
-var authorityUrl = authorityHostUrl + '/' + tenant;
-var clientId = 'yourClientIdHere';
-var clientSecret = 'yourAADIssuedClientSecretHere'
-var resource = '00000002-0000-0000-c000-000000000000';
- 
-var context = new AuthenticationContext(authorityUrl);
- 
-context.acquireTokenWithClientCredentials(resource, clientId, clientSecret, function(err, tokenResponse) {
-  if (err) {
-    console.log('well that didn\'t work: ' + err.stack);
-  } else {
-    console.log(tokenResponse);
-  }
-});
-
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Adal Node\n');
